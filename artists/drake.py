122fe6bdba14fd81891b7fb32ca7f8c0b47bf73c
Asad <muhammadasadk42@gmail.com>
2025-07-12 12:44:04 +0500

    Implementing and testing child function in main.py and also printing objects using show()/display() method

--- artists/drake.py
+++ artists/drake.py
@@ -3,10 +3,17 @@ class Drake:
     def __init__(self):
         self.name = "Aubrey 'Drake' Graham"
         self.genre = "Hip-Hop"
-        self.top_songs = ["God's Plan", "Hotline Bling", "Life is Good"]
+        self.top_songs = ["One Dance", "God's Plan", "Passionfruit"]
 
     def get_bio(self):
-        return f"{self.name} is a {self.genre} artist."
+        return f"{self.name} is a {self.genre} artist.\n \n"
 
     def respond(self, query):
         return self.get_bio()
+    
+    #Output objects using either display()/show() method
+    def display(self):
+        print(f"Artist : {self.name}")
+        print(f"Genre : {self.genre}")
+        print(f"Top 3 Most streamed songs : {self.top_songs}")
+              
--- main.py
+++ main.py
@@ -2,3 +2,9 @@ from artists.drake import Drake
 
 #Object creation (the 'new' keyword isn't conventionally present in Python)
 drake = Drake()
+
+#Child function and its functionality testing
+print(drake.get_bio())       # Calling child function get_bio()
+
+#Output objects using either display()/show() method
+drake.display()
