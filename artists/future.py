bc64561a6bbc8d9204b5246f5a756651041dbccb
Asad <muhammadasadk42@gmail.com>
2025-07-12 13:13:30 +0500

    Updated the other artist classes

---
+++ artists/future.py
@@ -0,0 +1,18 @@
+#Constructor used to create class
+class Drake:
+    def __init__(self):
+        self.name = "Future"
+        self.genre = "Hip-Hop/Trap"
+        self.top_songs = ["Mask Off ", "Life is Good ft. Drake", "Low Life ft. The Weeknd"]
+
+    def get_bio(self):
+        return f"{self.name} is a {self.genre} artist.\n \n"
+
+    def respond(self, query):
+        return self.get_bio()
+    
+    #Output objects using either display()/show() method
+    def display(self):
+        print(f"Artist : {self.name}")
+        print(f"Genre : {self.genre}")
+        print(f"Top 3 Most streamed songs : {self.top_songs}")
