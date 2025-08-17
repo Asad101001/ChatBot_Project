97d87a5aee8877964cb9443fc50b52e52391de66
Asad <muhammadasadk42@gmail.com>
2025-08-17 23:59:08 +0500

    Simplified main.py

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-from artists.carti import Carti
-from artists.drake import Drake
+#MINIMAL AND CLEAN main.py SERVING AS ENTRY POINT FOR CHATBOT WHILE FOLLOWING Single Responsibility Principle
 
-#Object creation (the 'new' keyword isn't conventionally present in Python)
-drake = Drake()
+from artists_interface.artists_interface import chatbot
 
-#Child function and its functionality testing
-print(drake.get_bio())       # Calling child function get_bio()
-
-#Output objects using either display()/show() method
-drake.display()
-
-#Creating second object this time by using class along with arguments
-carti = Carti("Playboi Carti", "Hip-Hop/Trap")
-print(carti.get_bio())
+if __name__ == "__main__":
+    chatbot()
 
